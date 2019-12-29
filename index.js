64c8e9e3580d14524c186f6c1fb857118b5379eb
tycho <tycho.holzer@gmail.com>
2019-12-29 12:38:24 +0100

    average review based only on verified reviews

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ const AmazonCrawler = require('./lib/AmazonCrawler');
 (async () => {
     const crawler = new AmazonCrawler();
     await crawler.init();
-    const profile = await crawler.crawlProfile("AFJWQ5SKSQ7H4OCHVMGZSPGUBGTA");
-    await crawler.stop();
+    /*const profile = await crawler.crawlProfile("AFJWQ5SKSQ7H4OCHVMGZSPGUBGTA");
 
     console.log("reviews: ", profile.reviewCount);
-    console.log("average review: ", profile.averageReview);
+    console.log("average review: ", profile.averageReview);*/
+    console.log(JSON.stringify(await crawler.crawlProductReviews("B00Y211AFM", 1, 1), null, 4));
+    await crawler.stop();
 })();
