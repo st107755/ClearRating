958c94f55b8d15e783898194797dba78a03f502d
Your Name <you@example.com>
2020-01-02 17:24:56 +0100

    not functioning mongo db connection

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const AmazonCrawler = require('./lib/AmazonCrawler');
+const DB = require('./lib/DbUtils')
+
 
 (async () => {
     const crawler = new AmazonCrawler();
+    const db = new DB();
     await crawler.init();
-    /*const profile = await crawler.crawlProfile("AFJWQ5SKSQ7H4OCHVMGZSPGUBGTA");
 
-    console.log("reviews: ", profile.reviewCount);
-    console.log("average review: ", profile.averageReview);*/
     const reviewerStatistics = await crawler.crawlProductReviews("B07GFC1RGW", 1, 1)
     console.log(JSON.stringify());
     await crawler.stop();
