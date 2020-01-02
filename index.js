1f129a548aceaf9770da1e3a127a0fa9f849ef55
tycho <tycho.holzer@gmail.com>
2020-01-02 17:40:20 +0100

    added tampermonkey script

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const AmazonCrawler = require('./lib/AmazonCrawler');
 
 (async () => {
-    const crawler = new AmazonCrawler();
+    const crawler = new AmazonCrawler(null, {
+        api: true
+    });
     await crawler.init();
     /*const profile = await crawler.crawlProfile("AFJWQ5SKSQ7H4OCHVMGZSPGUBGTA");
 
     console.log("reviews: ", profile.reviewCount);
     console.log("average review: ", profile.averageReview);*/
-    const reviewerStatistics = await crawler.crawlProductReviews("B07TWFWJDZ", 1, 1)
-    console.log(JSON.stringify());
-    await crawler.stop();
+    /*const reviewerStatistics = await crawler.crawlProductReviews("B07TWFWJDZ", 1, 1)
+    console.log(JSON.stringify(reviewerStatistics, null, 4));
+    await crawler.stop();*/
 })();
