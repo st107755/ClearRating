69d8f5fc32f30dd0021c66c436630b60c06fa1f6
tycho <tycho.holzer@gmail.com>
2020-01-03 18:07:29 +0100

    product review crawling

--- index.js
+++ index.js
@@ -6,11 +6,14 @@ const DB = require('./lib/DbUtils');
     const db = new DB();
     await crawler.init();
 
+    console.log(await crawler.crawlProductReviews("B08153RC4Q"));
+
+    /*
     const reviewerStatistics = await crawler.crawlProductReviews("B081V6J1KQ");
     reviewerStatistics.forEach(reviewer => {
         db.put(reviewer);
     });
 
-    console.log("sucessfull");
+    console.log("sucessfull");*/
     await crawler.stop();
 })();
