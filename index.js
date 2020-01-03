248773299ef61c832a4580907f56593c62b359f9
tycho <tycho.holzer@gmail.com>
2020-01-03 18:34:00 +0100

    proxy & db

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const AmazonCrawler = require('./lib/AmazonCrawler');
-const DB = require('./lib/DbUtils');
+const db = require('./lib/DbUtils');
 
 (async () => {
     const crawler = new AmazonCrawler();
-    const db = new DB();
     await crawler.init();
 
     console.log(await crawler.crawlProductReviews("B08153RC4Q"));
